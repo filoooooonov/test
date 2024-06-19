f31921e7ded2ae1808ec12edc5a58042a01e009d
Aleksei Filonov <alexfiloonov@gmail.com>
2024-06-19 15:20:09 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,12 +3,10 @@ const cors = require('cors');
 const app = express();
 const PORT = 5000;
 
-// app.use(cors())
+app.use(cors())
 
 
 app.use(express.json())
-
-
 
 
 app.post('/api', (req, res) => {
